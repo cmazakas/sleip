8c37e0143c2a3cfcad5d75cdd4fcdf10f3f1125c
LeonineKing1199 <christian.mazakas@gmail.com>
2019-12-07 18:27:07 -0800

    Add prelim move-assign tests

--- test/move_assignment.cpp
+++ test/move_assignment.cpp
@@ -7,6 +7,8 @@
 
 #include <algorithm>
 #include <vector>
+#include <array>
+#include <cstddef>
 #include <iostream>
 
 namespace pmr = boost::container::pmr;
@@ -15,6 +17,7 @@ void
 test_move_assignment_equal_allocators()
 {
   // empty
+  //
   {
     auto size = std::size_t{16};
 
@@ -35,6 +38,7 @@ test_move_assignment_equal_allocators()
   }
 
   // non-empty
+  //
   {
     auto size = std::size_t{16};
 
@@ -53,9 +57,69 @@ test_move_assignment_equal_allocators()
   }
 }
 
+void
+test_move_assignment_non_equal_allocator()
+{
+  // empty
+  //
+  {
+    auto mem  = std::array<std::byte, 4096>{};
+    auto buff = pmr::monotonic_buffer_resource(mem.data(), mem.size());
+
+    auto buff_alloc    = pmr::polymorphic_allocator<int>(&buff);
+    auto default_alloc = pmr::polymorphic_allocator<int>();
+
+    auto size = std::size_t{16};
+
+    auto a = sleip::dynamic_array<int, pmr::polymorphic_allocator<int>>(buff_alloc);
+    auto b = sleip::dynamic_array<int, pmr::polymorphic_allocator<int>>(2 * size, 1, default_alloc);
+
+    BOOST_TEST(a.get_allocator() != b.get_allocator());
+
+    auto old_data = b.data();
+
+    a = std::move(b);
+
+    BOOST_TEST_EQ(a.size(), 2 * size);
+
+    BOOST_TEST_EQ(b.data(), old_data);
+    BOOST_TEST_EQ(b.size(), 2 * size);
+  }
+
+  // non-empty
+  //
+  {
+    auto mem  = std::array<std::byte, 4096>{};
+    auto buff = pmr::monotonic_buffer_resource(mem.data(), mem.size());
+
+    auto buff_alloc    = pmr::polymorphic_allocator<std::vector<int>>(&buff);
+    auto default_alloc = pmr::polymorphic_allocator<std::vector<int>>();
+
+    auto size = std::size_t{4};
+
+    auto a = sleip::dynamic_array<std::vector<int>, pmr::polymorphic_allocator<std::vector<int>>>(
+      size, std::vector{1, 2, 3}, buff_alloc);
+
+    auto b = sleip::dynamic_array<std::vector<int>, pmr::polymorphic_allocator<std::vector<int>>>(
+      2 * size, std::vector{1, 2, 3}, default_alloc);
+
+    BOOST_TEST(a.get_allocator() != b.get_allocator());
+
+    auto old_data = b.data();
+
+    a = std::move(b);
+
+    BOOST_TEST_EQ(a.size(), 2 * size);
+
+    BOOST_TEST_EQ(b.data(), old_data);
+    BOOST_TEST_EQ(b.size(), 2 * size);
+  }
+}
+
 int
 main()
 {
   test_move_assignment_equal_allocators();
+  test_move_assignment_non_equal_allocator();
   return boost::report_errors();
 }
