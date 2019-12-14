d9f54184cee77394bb7b86f0955775339a487add
LeonineKing1199 <christian.mazakas@gmail.com>
2019-12-14 11:02:28 -0800

    Refactor header includes so the fwd header pulls in less

--- include/sleip/dynamic_array_fwd.hpp
+++ include/sleip/dynamic_array_fwd.hpp
@@ -1,23 +1,8 @@
 #ifndef SLEIP_DYNAMIC_ARRAY_FWD_HPP_
 #define SLEIP_DYNAMIC_ARRAY_FWD_HPP_
 
-#include <boost/assert.hpp>
-#include <boost/config.hpp>
-#include <boost/throw_exception.hpp>
-
-#include <boost/core/alloc_construct.hpp>
-#include <boost/core/empty_value.hpp>
 #include <boost/core/noinit_adaptor.hpp>
-#include <boost/core/pointer_traits.hpp>
-
-#include <boost/iterator/iterator_concepts.hpp>
-
-#include <algorithm>
-#include <cstddef>
-#include <iterator>
 #include <memory>
-#include <type_traits>
-#include <utility>
 
 namespace sleip
 {
