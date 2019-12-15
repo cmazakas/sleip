f42e4d7c2cd2d6a396e49d7a0c8f03afce1c0b03
LeonineKing1199 <christian.mazakas@gmail.com>
2019-12-14 17:22:58 -0800

    Create ::pmr typedefs on available platforms

--- include/sleip/dynamic_array_fwd.hpp
+++ include/sleip/dynamic_array_fwd.hpp
@@ -2,6 +2,14 @@
 #define SLEIP_DYNAMIC_ARRAY_FWD_HPP_
 
 #include <boost/core/noinit_adaptor.hpp>
+
+#if __has_include(<memory_resource>)
+#include <memory_resource>
+#define SLEIP_HAS_PMR 1
+#else
+#define SLEIP_HAS_PMR 0
+#endif
+
 #include <memory>
 
 namespace sleip
@@ -11,6 +19,19 @@ struct dynamic_array;
 
 template <class T, class Allocator = std::allocator<T>>
 using dynamic_array_noinit = dynamic_array<T, boost::noinit_adaptor<Allocator>>;
+
+#ifdef SLEIP_HAS_PMR
+namespace pmr
+{
+template <class T>
+using dynamic_array = ::sleip::dynamic_array<T, std::pmr::polymorphic_allocator<T>>;
+
+template <class T>
+using dynamic_array_noinit =
+  ::sleip::dynamic_array<T, boost::noinit_adaptor<std::pmr::polymorphic_allocator<T>>>;
+} // namespace pmr
+#endif
+#undef SLEIP_HAS_PMR
 
 template <class T, class Allocator>
 auto
