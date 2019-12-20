c1459813ecc97e17bba7f53e29013210bdbfb4e7
LeonineKing1199 <christian.mazakas@gmail.com>
2019-12-19 17:51:51 -0800

    Fix broken impl of PMR detection on msvc

--- include/sleip/dynamic_array_fwd.hpp
+++ include/sleip/dynamic_array_fwd.hpp
@@ -1,13 +1,20 @@
 #ifndef SLEIP_DYNAMIC_ARRAY_FWD_HPP_
 #define SLEIP_DYNAMIC_ARRAY_FWD_HPP_
 
+#if __has_include(<vcruntime.h>)
+#include <vcruntime.h>
+#endif
+
 #include <boost/config.hpp>
 
-#if !__has_include(<memory_resource>) || (__cplusplus < 201703L) || (BOOST_MSVC && !_HAS_CXX17)
+#if !__has_include(<memory_resource>) || (!BOOST_MSVC &&__cplusplus < 201703L) || (BOOST_MSVC && !_HAS_CXX17)
 #define SLEIP_NO_CXX17_PMR
 #endif
 
 #include <memory>
+#ifndef SLEIP_NO_CXX17_PMR
+#include <memory_resource>
+#endif
 
 namespace sleip
 {
@@ -19,13 +26,8 @@ namespace pmr
 {
 template <class T>
 using dynamic_array = ::sleip::dynamic_array<T, std::pmr::polymorphic_allocator<T>>;
-
-template <class T>
-using dynamic_array_noinit =
-  ::sleip::dynamic_array<T, boost::noinit_adaptor<std::pmr::polymorphic_allocator<T>>>;
 } // namespace pmr
 #endif
-#undef SLEIP_HAS_PMR
 
 template <class T, class Allocator>
 auto
