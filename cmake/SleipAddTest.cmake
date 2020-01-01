5cd19ff190e1efe1556b09a47636255cf6891b4c
LeonineKing1199 <christian.mazakas@gmail.com>
2020-01-01 08:25:38 -0800

    Refactor sleip_add_test to custom module

---
+++ cmake/SleipAddTest.cmake
@@ -0,0 +1,20 @@
+set(THREADS_PREFER_PTHREAD_FLAG ON CACHE BOOL "")
+
+find_package(Boost 1.71 REQUIRED container)
+find_package(Threads REQUIRED)
+
+function(sleip_add_test test_name)
+  add_executable(${test_name} "${test_name}.cpp")
+
+  target_link_libraries(${test_name} PRIVATE dynamic_array Boost::container Threads::Threads)
+
+  if (MSVC)
+    target_link_libraries(${test_name} PRIVATE Boost::disable_autolinking)
+  endif()
+
+  if (UNIX AND NOT APPLE)
+    target_link_libraries(${test_name} PRIVATE rt)
+  endif()
+
+  add_test(${test_name} ${test_name})
+endfunction()
